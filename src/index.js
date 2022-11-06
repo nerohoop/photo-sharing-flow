740bd86ba15def84c75ea438e2d21c80ba4649f6
Jiang Xiao <nerohoop@gmail.com>
2022-11-06 18:35:01 +0000

    Finish Google Auth login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
-ReactDom.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
